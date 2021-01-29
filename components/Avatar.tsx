571ab921dd6738bfa8e84296b8ae84150e1f8da2
Rishabh Jain <rishabh@codeation.io>
2021-01-29 18:44:29 +0530

    fix: Avatar component return type

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -1,4 +1,6 @@
-const Avatar = ({ children }) => {
+import { FC } from 'react';
+
+const Avatar: FC = ({ children }) => {
   return (
     <div className="rounded-full h-24 w-24 flex items-center justify-center m-4 border-dashed border bg-blue-400">
       {children}
