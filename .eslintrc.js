d57c907c405580db6eda532d47bbe2ea749b99b5
Rishabh Jain <rishabh@codeation.io>
2021-01-29 14:13:43 +0530

    fix: import sort error fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,7 +33,6 @@ module.exports = {
     'react/react-in-jsx-scope': 'off',
     'react/prop-types': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
-    'simple-import-sort/sort': 'error',
     'jsx-a11y/anchor-is-valid': [
       'error',
       {
