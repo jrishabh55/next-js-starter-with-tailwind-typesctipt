dea288dfb165a2770e4fea5eb0b9800f5710c517
Rishabh Jain <jrishabh55@gmail.com>
2022-05-01 00:32:43 +0530

    chore: added sort to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,9 @@
 module.exports = {
-  root: true,
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true
-    }
-  },
-  settings: {
-    react: {
-      version: 'detect'
-    }
-  },
   env: {
-    browser: true,
     amd: true,
+    browser: true,
     node: true
   },
-  plugins: ['simple-import-sort'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
@@ -28,20 +13,48 @@ module.exports = {
     'prettier',
     'plugin:prettier/recommended' // Make sure this is always the last element in the array.
   ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2020,
+    sourceType: 'module'
+  },
+  plugins: ['simple-import-sort', 'sort-destructure-keys'],
+  root: true,
   rules: {
-    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
-    'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     'jsx-a11y/anchor-is-valid': [
       'error',
       {
+        aspects: ['invalidHref', 'preferButton'],
         components: ['Link'],
-        specialLink: ['hrefLeft', 'hrefRight'],
-        aspects: ['invalidHref', 'preferButton']
+        specialLink: ['hrefLeft', 'hrefRight']
       }
     ],
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+    'react/jsx-sort-props': [
+      'error',
+      {
+        callbacksLast: true,
+        ignoreCase: false,
+        noSortAlphabetically: false,
+        reservedFirst: true,
+        shorthandFirst: true,
+        shorthandLast: false
+      }
+    ],
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'simple-import-sort/exports': 'error',
     'simple-import-sort/imports': 'error',
-    'simple-import-sort/exports': 'error'
+    'sort-destructure-keys/sort-destructure-keys': 2,
+    'sort-keys': ['error', 'asc', { caseSensitive: true, minKeys: 2, natural: false }]
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
   }
 };
